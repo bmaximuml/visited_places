d1233f99d4162ab9ffa352201ccf75b20e64c4b8
Max Levine <max@maxlevine.co.uk>
2020-11-30 22:51:44 +0000

    Add details for Max travels

--- countries.js
+++ countries.js
@@ -1,7 +1,21 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
+    CHE: {fillKey: 'visited'},
+    DEU: {fillKey: 'visited'},
+    DNK: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+    HRV: {fillKey: 'visited'},
+    IRL: {fillKey: 'visited'},
+    ISL: {fillKey: 'visited'},
+    ISR: {fillKey: 'visited'},
     ITA: {fillKey: 'visited'},
+    NLD: {fillKey: 'visited'},
+    NOR: {fillKey: 'visited'},
+    POL: {fillKey: 'visited'},
+    PRT: {fillKey: 'visited'},
+    SWE: {fillKey: 'visited'},
     USA: {fillKey: 'visited'},
-    THA: {fillKey: 'visited'},
 }
